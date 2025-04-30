050b6a5b8b5b75218c449da8bfc39d2b6996a04f
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-05-01 01:22:18 +0300

    update config

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,6 @@ import { getStoredTokens } from "@/services/auth";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_KEY,
   headers: {
     "Content-Type": "application/json",
     // ...(localStorage.getItem("x-branch-id") && {
@@ -13,6 +12,14 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
+    // const companyDomain = localStorage.getItem("company_domain");
+    config.baseURL =
+      "https://" + localStorage.getItem("company_domian") + ".eng-mahmoud.com";
+
+    // config.baseURL = companyDomain
+    //   ? `https://${companyDomain}.eng-mahmoud.com`
+    //   : "https://default.eng-mahmoud.com";
+
     const token = getStoredTokens();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -32,6 +39,7 @@ axiosInstance.interceptors.response.use(
   function (error) {
     if (error?.response?.data?.code === "token_not_valid") {
       localStorage.removeItem("auth_tokens");
+      localStorage.removeItem("company_domain");
       window.location.href = "/login";
     }
 
