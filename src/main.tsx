0f5673fedac9eecf373cae9b3a26ba657026b4ec
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-05-11 02:26:57 +0300

    sentry

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,26 @@ const queryClient = new QueryClient({
   },
 });
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://fc24befc39e8318d79dc6d7411d41d27@o4509301056995328.ingest.de.sentry.io/4509301074886736",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
