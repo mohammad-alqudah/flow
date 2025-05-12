7efde437985978577baf946304de80e3608d1903
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-05-12 04:25:23 +0300

    update

--- src/components/orders/FilterBar.tsx
+++ src/components/orders/FilterBar.tsx
@@ -2,32 +2,32 @@ import {
   Box,
   Input,
   Stack,
-  Text,
+  // Text,
   Accordion,
-  Grid,
-  GridItem,
+  // Grid,
+  // GridItem,
   InputGroup,
-  VStack,
-  HStack,
-  IconButton,
-  NativeSelect,
+  // VStack,
+  // HStack,
+  // IconButton,
+  // NativeSelect,
   Icon,
-  Field,
+  // Field,
 } from "@chakra-ui/react";
 import {
   Search,
-  Calendar,
-  Truck,
-  MapPin,
-  X,
-  FileText,
-  Package,
-  Users,
-  Briefcase,
+  // Calendar,
+  // Truck,
+  // MapPin,
+  // X,
+  // FileText,
+  // Package,
+  // Users,
+  // Briefcase,
 } from "lucide-react";
 // import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Tooltip } from "../ui/tooltip";
+// import { Tooltip } from "../ui/tooltip";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { FilterIcon } from "@hugeicons/core-free-icons";
 
@@ -77,174 +77,174 @@ interface FilterBarProps {
   };
 }
 
-const filterGroups = [
-  {
-    id: "basic",
-    title: "Basic Information",
-    icon: FileText,
-    filters: ["type", "mode", "declaration_number", "reference_number"],
-  },
-  {
-    id: "parties",
-    title: "Parties",
-    icon: Users,
-    filters: ["client", "shipper", "consignee", "notify"],
-  },
-  {
-    id: "locations",
-    title: "Locations",
-    icon: MapPin,
-    filters: ["pol", "pod", "pol_country", "pod_country", "final_destination"],
-  },
-  {
-    id: "dates",
-    title: "Dates",
-    icon: Calendar,
-    filters: ["etd", "eta", "created_at_range"],
-  },
-  {
-    id: "sea",
-    title: "Sea Freight",
-    icon: Package,
-    filters: ["mbl_number", "hbl_number", "shipping_line"],
-  },
-  {
-    id: "air",
-    title: "Air Freight",
-    icon: Truck,
-    filters: ["mawb_number", "hawb_number", "airline"],
-  },
-  {
-    id: "agents",
-    title: "Agents",
-    icon: Briefcase,
-    filters: ["clearing_agent", "agent", "network"],
-  },
-];
+// const filterGroups = [
+//   {
+//     id: "basic",
+//     title: "Basic Information",
+//     icon: FileText,
+//     filters: ["type", "mode", "declaration_number", "reference_number"],
+//   },
+//   {
+//     id: "parties",
+//     title: "Parties",
+//     icon: Users,
+//     filters: ["client", "shipper", "consignee", "notify"],
+//   },
+//   {
+//     id: "locations",
+//     title: "Locations",
+//     icon: MapPin,
+//     filters: ["pol", "pod", "pol_country", "pod_country", "final_destination"],
+//   },
+//   {
+//     id: "dates",
+//     title: "Dates",
+//     icon: Calendar,
+//     filters: ["etd", "eta", "created_at_range"],
+//   },
+//   {
+//     id: "sea",
+//     title: "Sea Freight",
+//     icon: Package,
+//     filters: ["mbl_number", "hbl_number", "shipping_line"],
+//   },
+//   {
+//     id: "air",
+//     title: "Air Freight",
+//     icon: Truck,
+//     filters: ["mawb_number", "hawb_number", "airline"],
+//   },
+//   {
+//     id: "agents",
+//     title: "Agents",
+//     icon: Briefcase,
+//     filters: ["clearing_agent", "agent", "network"],
+//   },
+// ];
 
 export function FilterBar({
   filters,
   onFilterChange,
-  visibleFilters,
-  filterOptions,
-}: FilterBarProps) {
-  const handleClearFilter = (filterKey: string) => {
-    onFilterChange({
-      ...filters,
-      [filterKey]: Array.isArray(filters[filterKey]) ? [null, null] : "",
-    });
-  };
+}: // visibleFilters,
+// filterOptions,
+FilterBarProps) {
+  // const handleClearFilter = (filterKey: string) => {
+  //   onFilterChange({
+  //     ...filters,
+  //     [filterKey]: Array.isArray(filters[filterKey]) ? [null, null] : "",
+  //   });
+  // };
 
-  const renderFilterInput = (filterId: string) => {
-    const label = filterId
-      .split("_")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+  // const renderFilterInput = (filterId: string) => {
+  //   const label = filterId
+  //     .split("_")
+  //     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  //     .join(" ");
 
-    if (filterId === "type" || filterId === "mode") {
-      return (
-        <NativeSelect.Root>
-          <NativeSelect.Field
-            value={filters[filterId]}
-            onChange={(e) =>
-              onFilterChange({ ...filters, [filterId]: e.target.value })
-            }
-            placeholder={`All ${label}s`}
-          >
-            {filterOptions[filterId]?.map((option: string) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </NativeSelect.Field>
-        </NativeSelect.Root>
-      );
-    }
+  //   if (filterId === "type" || filterId === "mode") {
+  //     return (
+  //       <NativeSelect.Root>
+  //         <NativeSelect.Field
+  //           value={filters[filterId]}
+  //           onChange={(e) =>
+  //             onFilterChange({ ...filters, [filterId]: e.target.value })
+  //           }
+  //           placeholder={`All ${label}s`}
+  //         >
+  //           {filterOptions[filterId]?.map((option: string) => (
+  //             <option key={option} value={option}>
+  //               {option}
+  //             </option>
+  //           ))}
+  //         </NativeSelect.Field>
+  //       </NativeSelect.Root>
+  //     );
+  //   }
 
-    if (filterId === "etd" || filterId === "eta") {
-      return (
-        <Input
-          type="date"
-          value={filters[filterId]}
-          onChange={(e) =>
-            onFilterChange({ ...filters, [filterId]: e.target.value })
-          }
-        />
-      );
-    }
+  //   if (filterId === "etd" || filterId === "eta") {
+  //     return (
+  //       <Input
+  //         type="date"
+  //         value={filters[filterId]}
+  //         onChange={(e) =>
+  //           onFilterChange({ ...filters, [filterId]: e.target.value })
+  //         }
+  //       />
+  //     );
+  //   }
 
-    if (filterId === "created_at_range") {
-      return (
-        <VStack gap={2} w={"full"} align="stretch">
-          {/* <DatePicker
-            selected={filters[filterId][0]}
-            onChange={(date) =>
-              onFilterChange({
-                ...filters,
-                [filterId]: [date, filters[filterId][1]],
-              })
-            }
-            customInput={<Input w={"full"} />}
-            placeholderText="Start Date"
-            isClearable
-          /> */}
-          <Field.Root>
-            <Field.Label>Start date</Field.Label>
-            <Input
-              type="date"
-              value={filters[filterId][0] ?? ""}
-              onChange={(e) =>
-                onFilterChange({
-                  ...filters,
-                  [filterId]: [e.target.value, filters[filterId][1]],
-                })
-              }
-            />
-          </Field.Root>
-          {/* <DatePicker
-            selected={filters[filterId][1]}
-            onChange={(date) =>
-              onFilterChange({
-                ...filters,
-                [filterId]: [filters[filterId][0], date],
-              })
-            }
-            customInput={<Input />}
-            placeholderText="End Date"
-            isClearable
-          /> */}
-          <Field.Root>
-            <Field.Label>End date</Field.Label>
-            <Input
-              type="date"
-              value={filters[filterId][1] ?? ""}
-              onChange={(e) =>
-                onFilterChange({
-                  ...filters,
-                  [filterId]: [filters[filterId][0], e.target.value],
-                })
-              }
-            />
-          </Field.Root>
-        </VStack>
-      );
-    }
+  //   if (filterId === "created_at_range") {
+  //     return (
+  //       <VStack gap={2} w={"full"} align="stretch">
+  //         {/* <DatePicker
+  //           selected={filters[filterId][0]}
+  //           onChange={(date) =>
+  //             onFilterChange({
+  //               ...filters,
+  //               [filterId]: [date, filters[filterId][1]],
+  //             })
+  //           }
+  //           customInput={<Input w={"full"} />}
+  //           placeholderText="Start Date"
+  //           isClearable
+  //         /> */}
+  //         <Field.Root>
+  //           <Field.Label>Start date</Field.Label>
+  //           <Input
+  //             type="date"
+  //             value={filters[filterId][0] ?? ""}
+  //             onChange={(e) =>
+  //               onFilterChange({
+  //                 ...filters,
+  //                 [filterId]: [e.target.value, filters[filterId][1]],
+  //               })
+  //             }
+  //           />
+  //         </Field.Root>
+  //         {/* <DatePicker
+  //           selected={filters[filterId][1]}
+  //           onChange={(date) =>
+  //             onFilterChange({
+  //               ...filters,
+  //               [filterId]: [filters[filterId][0], date],
+  //             })
+  //           }
+  //           customInput={<Input />}
+  //           placeholderText="End Date"
+  //           isClearable
+  //         /> */}
+  //         <Field.Root>
+  //           <Field.Label>End date</Field.Label>
+  //           <Input
+  //             type="date"
+  //             value={filters[filterId][1] ?? ""}
+  //             onChange={(e) =>
+  //               onFilterChange({
+  //                 ...filters,
+  //                 [filterId]: [filters[filterId][0], e.target.value],
+  //               })
+  //             }
+  //           />
+  //         </Field.Root>
+  //       </VStack>
+  //     );
+  //   }
 
-    return (
-      <Input
-        placeholder={`Filter by ${label.toLowerCase()}...`}
-        value={filters[filterId]}
-        onChange={(e) =>
-          onFilterChange({ ...filters, [filterId]: e.target.value })
-        }
-      />
-    );
-  };
+  //   return (
+  //     <Input
+  //       placeholder={`Filter by ${label.toLowerCase()}...`}
+  //       value={filters[filterId]}
+  //       onChange={(e) =>
+  //         onFilterChange({ ...filters, [filterId]: e.target.value })
+  //       }
+  //     />
+  //   );
+  // };
 
   // Filter out groups with no visible filters
-  const activeFilterGroups = filterGroups.filter((group) =>
-    group.filters.some((filterId) => visibleFilters[filterId])
-  );
+  // const activeFilterGroups = filterGroups.filter((group) =>
+  //   group.filters.some((filterId) => visibleFilters[filterId])
+  // );
 
   return (
     <Accordion.Root
@@ -280,7 +280,7 @@ export function FilterBar({
                     />
                   </InputGroup>
 
-                  <Accordion.Root
+                  {/* <Accordion.Root
                     defaultValue={activeFilterGroups.map(
                       (_, index) => `${index}`
                     )}
@@ -357,7 +357,7 @@ export function FilterBar({
                         </Accordion.Item>
                       );
                     })}
-                  </Accordion.Root>
+                  </Accordion.Root> */}
                 </Stack>
               </Box>
               {/* filters */}
